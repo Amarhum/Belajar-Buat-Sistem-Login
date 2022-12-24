b726313b7e3821095bafea3665e07835239c4d02
Amarhum <muamar171215@gmail.com>
2022-12-23 20:26:37 -0800

    pembaruan

--- LogIn.java
+++ LogIn.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 public class LogIn{
 
@@ -11,22 +12,22 @@ public class LogIn{
         sc = new Scanner(System.in);
 
             //MEMBUAT Id
-        System.out.println("----- MEMBUAT AKUN----");
+        System.out.println("----- BUAT AKUN----");
         System.out.println("Buat Id : ");
         id = sc.nextLine();
         // Membuat Password
         System.out.println("Buat Password : ");
         pw = sc.nextLine();
         // OutPut Yang Disimpan
-        System.out.println("-----------------------");
+        System.out.println("----- SUKSES MEMBUAT AKUN ------");
         System.out.println("Kode Pengguna :" + id.hashCode());
         System.out.println("Id anda : " + id);
         System.out.println("password anda : " + pw);
 
-
-            //LOGIN 
+            
         String id2=id;
         String pw2 = pw; 
+            //LOGIN 
         System.out.println("-----LOGIN------");
         System.out.println("Masukan Id : "); 
         id2 = sc.nextLine();
@@ -34,22 +35,34 @@ public class LogIn{
         System.out.println("Masukan Password : "); 
         pw2 = sc.nextLine();
 
-        if(id2.equals(id)){
-            System.out.println("Id yang anda Masukan Benar");
-        }
-        else{
-            System.out.println("Id Yang Anda Masukan Salah");
-        }
-        
-        
-        if(pw2.equals(pw)){
-            System.out.println("password yang anda Masukan Benar");
-        }
-        else{
-            System.out.println("password Yang Anda Masukan Salah");
-        }
 
 
-
+        if(id2.equals(id) && pw2.equals(pw)){
+            System.out.println("Login Sukses");
+        }
+        else if(id2 != id && pw2.equals(pw)){
+            System.out.println("ID yang Anda Masukan Salah ");
+        }
+        else if(pw2 != pw && id2.equals(id)){
+            System.out.println("Password yang Anda Masukan Salah ");
+        }
+        else {
+            System.out.println(" ID dan password Yang Anda Masukan Salah");
+        }
     }
 }
+        // if(id2.equals(id) && pw2.equals(pw)){
+        //     System.out.println("Login Sukses");
+        // }
+        // else {
+        //     if(id2 != id && pw2.equals(pw)){
+        //         System.out.println("ID yang Anda Masukan Salah ");
+        //     }
+        //     if(pw2 != pw && id2.equals(id)){
+        //         System.out.println("Password yang Anda Masukan Salah ");
+        //     }
+        //     else {
+        //             System.out.println(" ID dan password Yang Anda Masukan Salah");
+        //         }
+        // }
+  
